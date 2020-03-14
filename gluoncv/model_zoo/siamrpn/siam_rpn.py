b668f104996532b35509e39d307835fc79b9ec2f
Ubuntu <ubuntu@ip-172-31-22-213.us-east-2.compute.internal>
2020-03-14 05:17:16 +0000

    change RPN will accept different shape with train and test

--- gluoncv/model_zoo/siamrpn/siam_rpn.py
+++ gluoncv/model_zoo/siamrpn/siam_rpn.py
@@ -7,7 +7,8 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from mxnet.gluon.block import HybridBlock
 from mxnet.gluon import nn
-
+from mxnet.context import cpu
+import pdb
 class RPN(HybridBlock):
     """RPN head"""
     def __init__(self):
@@ -32,7 +33,7 @@ class DepthwiseXCorr(HybridBlock):
         kernel_size : float
             hidden kernel size
     """
-    def __init__(self, hidden, out_channels, kernel_size=3):
+    def __init__(self, hidden, out_channels, bz=1, if_train=False, kernel_size=3, ctx=cpu()):
         super(DepthwiseXCorr, self).__init__()
         self.conv_kernel = nn.HybridSequential(prefix='')
         self.conv_search = nn.HybridSequential(prefix='')
@@ -48,9 +49,18 @@ class DepthwiseXCorr(HybridBlock):
                       nn.BatchNorm(),
                       nn.Activation('relu'),
                       nn.Conv2D(out_channels, kernel_size=1))
-        self.kernel_size = [1, 256, 4, 4]
-        self.search_size = [1, 256, 24, 24]
-        self.out = [1, 256, 21, 21]
+        if if_train:
+            self.kernel_size = [bz, 256, 4, 4]
+            self.search_size = [bz, 256, 20, 20]
+            self.out_size = [bz, 256, 17, 17]
+        else:
+            self.kernel_size = [1, 256, 4, 4]
+            self.search_size = [1, 256, 20, 20]
+            self.out_size = [1, 256, 17, 17]
+
+        self.conv_kernel.initialize(ctx=ctx)
+        self.conv_search.initialize(ctx=ctx)
+        self.head.initialize(ctx=ctx)
 
     def hybrid_forward(self, F, kernel, search):
         """hybrid_forward"""
@@ -58,13 +68,11 @@ class DepthwiseXCorr(HybridBlock):
         search = self.conv_search(search)#[1, 256, 24, 24]
         batch = self.kernel_size[0]
         channel = self.kernel_size[1]
-        w = self.kernel_size[2]
-        h = self.kernel_size[3]
-        search = search.reshape((1, batch*channel, self.search_size[2], self.search_size[3]))
-        kernel = kernel.reshape((batch*channel, 1, self.kernel_size[2], self.kernel_size[3]))
-        out = F.Convolution(data=search, weight=kernel, kernel=[h, w], no_bias=True,
-                            num_filter=channel, num_group=batch*channel)
-        out = out.reshape((batch, channel, self.out[2], self.out[3]))#[1, 256, 21, 21]
+        search = search.reshape((1, -1, self.search_size[2], self.search_size[3]))
+        kernel = kernel.reshape((-1, 1, self.kernel_size[2], self.kernel_size[3]))
+        out = F.Convolution(data=search, weight=kernel, kernel=[self.kernel_size[2], self.kernel_size[3]],
+                            no_bias=True, num_filter=batch*channel, num_group=batch*channel)
+        out = out.reshape((batch, channel, self.out_size[2], self.out_size[3]))#[1, 256, 21, 21]
         out = self.head(out)
         return out
 
@@ -79,12 +87,13 @@ class DepthwiseRPN(RPN):
         out_channels : int
             hidden feature channel
     """
-    def __init__(self, anchor_num=5, out_channels=256):
+    def __init__(self, bz=1, if_train=False, ctx=cpu(), anchor_num=5, out_channels=256):
         super(DepthwiseRPN, self).__init__()
-        self.cls = DepthwiseXCorr(out_channels, 2 * anchor_num)
-        self.loc = DepthwiseXCorr(out_channels, 4 * anchor_num)
+        self.cls = DepthwiseXCorr(out_channels, 2 * anchor_num, bz=bz, if_train=if_train, ctx=ctx)
+        self.loc = DepthwiseXCorr(out_channels, 4 * anchor_num, bz=bz, if_train=if_train, ctx=ctx)
 
     def hybrid_forward(self, F, z_f, x_f):
         cls = self.cls(z_f, x_f)
         loc = self.loc(z_f, x_f)
         return cls, loc
+
